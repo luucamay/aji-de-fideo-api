f4529a516e43a963cb6e70f62de1853f3b3aec12
Lupe <lupemaydana@gmail.com>
2020-10-31 23:36:01 -0400

    feat: Server returns CSV data

--- csv.js
+++ csv.js
@@ -3,7 +3,12 @@ const http = require("http");
 const host = 'localhost';
 const port = 8000;
 
-const requestListener = function (req, res) {};
+const requestListener = function (req, res) {
+    res.setHeader("Content-Type", "text/csv");
+    res.setHeader("Content-Disposition", "attachment;filename=oceanpals.csv");
+    res.writeHead(200);
+    res.end(`id,name,email\n1,Sammy Shark,shark@ocean.com`);
+};
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
