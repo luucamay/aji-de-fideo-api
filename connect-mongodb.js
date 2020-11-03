a08b219d99d6b4e094ea1215113d3b873b3736eb
Lupe <lupemaydana@gmail.com>
2020-11-03 15:04:04 -0400

    feat: First connection to Mongo Atlas Database

---
+++ connect-mongodb.js
@@ -0,0 +1,36 @@
+const {MongoClient} = require('mongodb');
+const dotenv = require('dotenv');
+dotenv.config();
+
+async function main(){
+  /**
+   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+   */
+  const uri = process.env.DB_URL || 'mongodb://localhost:27017/test';
+
+
+  const client = new MongoClient(uri);
+
+  try {
+      // Connect to the MongoDB cluster
+      await client.connect();
+
+      // Make the appropriate DB calls
+      await  listDatabases(client);
+
+  } catch (e) {
+      console.error(e);
+  } finally {
+      await client.close();
+  }
+}
+
+async function listDatabases(client){
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+main().catch(console.error);
