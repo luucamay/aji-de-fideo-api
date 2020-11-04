deae3a22ead12aa755f3b30173a0b46d786a7bf2
Lupe <lupemaydana@gmail.com>
2020-11-04 00:29:34 -0400

    feat: Read documents from a collection in mongoDB

--- LEARNING- NOTES.md
+++ LEARNING- NOTES.md
@@ -2,3 +2,6 @@
 
 ## Create a database in MongoDB
 A “capped collection” has a maximum document count that prevents overflowing documents.
+
+## Read a collection
+Whenever you want to read a collection and you don't need to filter the data you just use the ```find``` command with no arguments
--- connect-mongodb.js
+++ connect-mongodb.js
@@ -1,8 +1,8 @@
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
 const dotenv = require('dotenv');
 dotenv.config();
 
-async function main(){
+async function main() {
   /**
    * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
    * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
@@ -10,27 +10,34 @@ async function main(){
   const uri = process.env.DB_URL || 'mongodb://localhost:27017/test';
 
 
-  const client = new MongoClient(uri);
-
+  const client = new MongoClient(uri, { useUnifiedTopology: true });
   try {
-      // Connect to the MongoDB cluster
-      await client.connect();
+    // Connect to the MongoDB cluster
+    await client.connect();
 
-      // Make the appropriate DB calls
-      await  listDatabases(client);
+    // Make the appropriate DB calls
+    // await listDatabases(client);
+    await readCollection(client);
 
   } catch (e) {
-      console.error(e);
+    console.error(e);
   } finally {
-      await client.close();
+    await client.close();
   }
 }
 
-async function listDatabases(client){
+async function listDatabases(client) {
   databasesList = await client.db().admin().listDatabases();
 
   console.log("Databases:");
   databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 };
+
+async function readCollection(client) {
+  const coll = client.db('ajidefideo').collection('products').find();
+
+  const results = await coll.toArray();
+  results.forEach(console.log)
+}
 
 main().catch(console.error);
