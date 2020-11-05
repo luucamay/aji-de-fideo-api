b23d77c74cf46cca436c9a925db00979819b6e7a
Lupe <lupemaydana@gmail.com>
2020-11-04 21:54:39 -0400

    feat: First routes

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 const express = require('express');
 const app = express();
+
+app.get('/', function(req, res) {
+  res.send("Yep it's working");
+});
+
+app.get('/love', (req, res) => {
+  res.send('Hi Love');
+});
+
+app.listen(3000, function() {
+  console.log('listening on 3000');
+});
