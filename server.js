aec92760d43cdf55f5c07ca3dc8ff4d285ade8e3
Lupe <lupemaydana@gmail.com>
2020-11-09 19:28:05 -0400

    feat: End request/response cycle if user is not set on your req object

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+
 const setCurrentUser = require('./middleware/setCurrentUser.js');
+const isLoggedIn = require("./middleware/isLoggedIn.js");
 
 const app = express();
 
 app.use(setCurrentUser);
 
-// ...
+app.get("/users", isLoggedIn, function(req, res) {
+  // get users...
+});
