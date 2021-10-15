3bbd01fb15b64fe06cef3f931845f8dea08eb719
luucamay <lupemaydana@gmail.com>
2021-10-14 21:18:54 -0700

    call to external api

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+  let num = Math.floor((Math.random() * 10) + 1);
+  // Make GET Request on every 2 second
+  axios.get(
+    `https://jsonplaceholder.typicode.com/posts/${num}`)
+
+    // Print data
+    .then(response => {
+      const { id, title } = response.data
+      console.log(`Post ${id}: ${title}\n`)
+    })
+
+    // Print error message if occur
+    .catch(error => console.log(
+      'Error to fetch data\n'))
   res.send('Hello World!')
 })
 
